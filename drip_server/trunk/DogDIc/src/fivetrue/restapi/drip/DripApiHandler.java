684e2c07290f39d9b68ad80e2700b3d044b6067a
dudrpdjwls <dudrpdjwls@e9a08410-762d-0510-9cfe-9c7b6143a7ee>
2016-03-13 15:44:31 +0000

    Config관련 API 작업 시작. 모든 API에 Header 값 확인 하도록 변경.

--- drip_server/trunk/DogDIc/src/fivetrue/restapi/drip/DripApiHandler.java
+++ drip_server/trunk/DogDIc/src/fivetrue/restapi/drip/DripApiHandler.java
@@ -32,100 +32,100 @@ public class DripApiHandler extends BaseApiHandler{
 	}
 
 	public void likeDrip(){
-		String id = getParameter(ID);
-		String author = getParameter(AUTHOR);
-		String user = getParameter(USER);
-		
-		boolean isValidId = id != null && id.length() > 0;
-		boolean isValidAuthor = author != null && author.length() > 0;
-		boolean isValidUser = user != null && user.length() > 0;
-		Result result = new Result();
-		if(isValidAuthor && isValidId && isValidUser){
-			Users authorUser = UsersManager.getInstance().getUser(author);
-			Users requestUser = UsersManager.getInstance().getUser(user);
+		if(checkRequestValidation()){
+			String id = getParameter(ID);
+			String author = getParameter(AUTHOR);
+			String user = getParameter(USER);
 			
-			
-		}else{
-			result.setErrorCode(Result.ERROR_CODE_REQUEST_ERROR);
-			result.setMessage("작성자 계정 또는 유저 계정 또는 드립 ID가 올바르지 않습니다.");
+			boolean isValidId = id != null && id.length() > 0;
+			boolean isValidAuthor = author != null && author.length() > 0;
+			boolean isValidUser = user != null && user.length() > 0;
+			Result result = new Result();
+			if(isValidAuthor && isValidId && isValidUser){
+				Users authorUser = UsersManager.getInstance().getUser(author);
+				Users requestUser = UsersManager.getInstance().getUser(user);
+				
+				
+			}else{
+				result.setErrorCode(Result.ERROR_CODE_REQUEST_ERROR);
+				result.setMessage("작성자 계정 또는 유저 계정 또는 드립 ID가 올바르지 않습니다.");
+			}
+			result.makeResponseTime();
+			writeObject(result);
 		}
-		result.makeResponseTime();
-		writeObject(result);
-		
-		
-		
-		
-		
 	}
 
 	public void readDrips(){
-		String id = getParameter(ID);
-		String drip = getParameter(DRIP);
-		String author = getParameter(AUTHOR);
-
-		List<Drips> drips = DripsManager.getInstance().getDrips(id, author, drip);
-		Result result = new Result();
-		result.setErrorCode(Result.ERROR_CODE_OK);
-		result.setResult(drips);
-		result.makeResponseTime();
-		writeObject(result);
+		if(checkRequestValidation()){
+			String id = getParameter(ID);
+			String drip = getParameter(DRIP);
+			String author = getParameter(AUTHOR);
+			
+			List<Drips> drips = DripsManager.getInstance().getDrips(id, author, drip);
+			Result result = new Result();
+			result.setErrorCode(Result.ERROR_CODE_OK);
+			result.setResult(drips);
+			result.makeResponseTime();
+			writeObject(result);
+		}
 	}
 	
 	public void putDrip(){
-		
-		String drip = getParameter(DRIP);
-		String author = getParameter(AUTHOR);
-		
-		Result result = new Result();
-		
-		boolean isValidDrip = drip != null && drip.length() > 0;
-		boolean isValidAuthor = author != null && author.length() > 0;
-		
-		if(isValidDrip && isValidAuthor){
+		if(checkRequestValidation()){
+			String drip = getParameter(DRIP);
+			String author = getParameter(AUTHOR);
 			
-			/**
-			 * 작성자가회원인지 체크.
-			 */
-			if(UsersManager.getInstance().isValidUser(author)){
+			Result result = new Result();
+			
+			boolean isValidDrip = drip != null && drip.length() > 0;
+			boolean isValidAuthor = author != null && author.length() > 0;
+			
+			if(isValidDrip && isValidAuthor){
+				
 				/**
-				 * 중복된 드립인 체크.
+				 * 작성자가회원인지 체크.
 				 */
-				if(DripsManager.getInstance().checkDrip(drip)){
-					Drips drips = new Drips();
-					drips.setCreatedate(System.currentTimeMillis());
-					drips.setDrip(drip);
-					drips.setAuthor(author);
-					drips.setHeartcount(0);
-					DBMessage dbMsg = DripsManager.getInstance().insertObject(drips);
-					if(dbMsg.getRow() > 0){
-						result.setErrorCode(Result.ERROR_CODE_OK);
-						result.setResult(drips);		
+				if(UsersManager.getInstance().isValidUser(author)){
+					/**
+					 * 중복된 드립인 체크.
+					 */
+					if(DripsManager.getInstance().checkDrip(drip)){
+						Drips drips = new Drips();
+						drips.setCreatedate(System.currentTimeMillis());
+						drips.setDrip(drip);
+						drips.setAuthor(author);
+						drips.setHeartcount(0);
+						DBMessage dbMsg = DripsManager.getInstance().insertObject(drips);
+						if(dbMsg.getRow() > 0){
+							result.setErrorCode(Result.ERROR_CODE_OK);
+							result.setResult(drips);		
+						}else{
+							result.setErrorCode(Result.ERROR_CODE_DB_ERROR);
+							result.setMessage(dbMsg.getMessage());
+							result.setResult(dbMsg);
+						}
 					}else{
-						result.setErrorCode(Result.ERROR_CODE_DB_ERROR);
-						result.setMessage(dbMsg.getMessage());
-						result.setResult(dbMsg);
+						result.setErrorCode(ERROR_CODE_DUPLICATED_DRIP);
+						result.setMessage("중복된 드립입니다.");
 					}
 				}else{
-					result.setErrorCode(ERROR_CODE_DUPLICATED_DRIP);
-					result.setMessage("중복된 드립입니다.");
+					result.setErrorCode(ERROR_CODE_INVALID_AUTHOR);
+					result.setMessage("작성자가 존재하지 않는 회원입니다.");
 				}
+				
 			}else{
-				result.setErrorCode(ERROR_CODE_INVALID_AUTHOR);
-				result.setMessage("작성자가 존재하지 않는 회원입니다.");
+				result.setErrorCode(Result.ERROR_CODE_REQUEST_ERROR);
+				if(!isValidAuthor && !isValidDrip){
+					result.setMessage("드립과 작성자가 입력되지 않았습니다.");
+				}else if(!isValidAuthor){
+					result.setMessage("작성자가 입력되지 않았습니다.");
+				}else if(!isValidDrip){
+					result.setMessage("드립이 입력되지 않았습니다.");
+				}
 			}
-			
-		}else{
-			result.setErrorCode(Result.ERROR_CODE_REQUEST_ERROR);
-			if(!isValidAuthor && !isValidDrip){
-				result.setMessage("드립과 작성자가 입력되지 않았습니다.");
-			}else if(!isValidAuthor){
-				result.setMessage("작성자가 입력되지 않았습니다.");
-			}else if(!isValidDrip){
-				result.setMessage("드립이 입력되지 않았습니다.");
-			}
+			result.makeResponseTime();
+			writeObject(result);
 		}
-		result.makeResponseTime();
-		writeObject(result);
 	}
 
 }
--- drip_server/trunk/DogDIc/src/fivetrue/restapi/user/UserApiHandler.java
+++ drip_server/trunk/DogDIc/src/fivetrue/restapi/user/UserApiHandler.java
@@ -37,136 +37,140 @@ public class UserApiHandler extends BaseApiHandler{
 	}
 	
 	public void registerUser(){
-		String email = getParameter(EMAIL);
-		String password = getParameter(PASSWORD);
-		String nickname = getParameter(NICKNAME);
-		
-		boolean isValidEmail = UsersManager.isValidEmail(email);
-		boolean isValidPassword = UsersManager.isValidPassword(password);
-		boolean isValidNickname = nickname != null && nickname.length() > 0;
-		
-		Result result = new Result();
-		
-		if(isValidEmail && isValidPassword && isValidNickname){
-			boolean checkEmail = UsersManager.getInstance().checkUserEmail(email);
-			if(!checkEmail){
-				result.setErrorCode(ERROR_CODE_DUPLICATED_EMAIL);
-				result.setMessage("중복된 이메일 입니다.");
-				result.makeResponseTime();
-				writeObject(result);
-				return;
-			}
+		if(checkRequestValidation()){
+			String email = getParameter(EMAIL);
+			String password = getParameter(PASSWORD);
+			String nickname = getParameter(NICKNAME);
 			
-			boolean checkNickname = UsersManager.getInstance().checkUserNickname(nickname);
-			if(!checkNickname){
-				result.setErrorCode(ERROR_CODE_DUPLICATED_NICKNAME);
-				result.setMessage("중복된 닉네임 입니다.");
-				result.makeResponseTime();
-				writeObject(result);
-				return;
-			}
+			boolean isValidEmail = UsersManager.isValidEmail(email);
+			boolean isValidPassword = UsersManager.isValidPassword(password);
+			boolean isValidNickname = nickname != null && nickname.length() > 0;
 			
-			Users user = new Users();
-			user.setCreatedate(System.currentTimeMillis());
-			user.setEmail(email);
-			user.setNickname(nickname);
-			user.setPassword(password);
-			user.setPoint(0);
-			DBMessage dbMsg = UsersManager.getInstance().insertObject(user);
+			Result result = new Result();
 			
-			
-			if(dbMsg.getRow() > 0){
-				result.setMessage(Result.OK_MESSAGE);
-				result.setErrorCode(Result.ERROR_CODE_OK);
-				result.setResult(user);	
-			}else{
-				result.setMessage(dbMsg.getMessage());
-				result.setErrorCode(Result.ERROR_CODE_DB_ERROR);
-				result.setResult(user);
-			}
-			result.makeResponseTime();
-			writeObject(result);
-			
-		}else{
-			result.setErrorCode(Result.ERROR_CODE_REQUEST_ERROR);
-			String message = null;
-			
-			if(!isValidEmail && !isValidPassword && isValidNickname){
-				message = "이메일 형식이 올바르지 않습니다. 비밀번호는 8자리 이상 16자리 미만입니다. 닉네임이 올바르지 않습니다.";
-			}else if(!isValidEmail){
-				message = "이메일 형식이 올바르지 않습니다.";
-			}else if(!isValidPassword){
-				message = "비밀번호는 8자리 이상 16자리 미만입니다.";
-			}else if(!isValidNickname){
-				message = "닉네임이 올바르지 않습니다.";
-			}
-			result.setMessage(message);
-			result.makeResponseTime();
-			writeObject(result);
-		}
-	}
-	
-	public void loginUser(){
-		String email = getParameter(EMAIL);
-		String password = getParameter(PASSWORD);
-		String device = getParameter(DEVICE);
-		
-		boolean isValidEmail = UsersManager.isValidEmail(email);
-		boolean isValidPassword = UsersManager.isValidPassword(password);
-		boolean isValidDevice = device != null && device.length() > 0;
-		
-		Result result = new Result();
-		
-		if(isValidEmail && isValidPassword && isValidDevice){
-			Users user = UsersManager.getInstance().checkUser(email, password);
-			if(user != null){
-				/**
-				 * 일일 첫 방문 시 포인트 증가 로직.
-				 */
-				long currentConnTimestamp = System.currentTimeMillis();
-				long lastDays =  TimeUnit.MILLISECONDS.toDays(user.getLastconn());
-				long currentDays = TimeUnit.MILLISECONDS.toDays(currentConnTimestamp);
-				
-				if(currentDays > lastDays){
-					user.setPoint(user.getPoint() + 1);
+			if(isValidEmail && isValidPassword && isValidNickname){
+				boolean checkEmail = UsersManager.getInstance().checkUserEmail(email);
+				if(!checkEmail){
+					result.setErrorCode(ERROR_CODE_DUPLICATED_EMAIL);
+					result.setMessage("중복된 이메일 입니다.");
+					result.makeResponseTime();
+					writeObject(result);
+					return;
 				}
 				
-				user.setLastconn(currentConnTimestamp);
-				user.setDevice(device);
-				DBMessage dbMsg = UsersManager.getInstance().updateObject(user);
+				boolean checkNickname = UsersManager.getInstance().checkUserNickname(nickname);
+				if(!checkNickname){
+					result.setErrorCode(ERROR_CODE_DUPLICATED_NICKNAME);
+					result.setMessage("중복된 닉네임 입니다.");
+					result.makeResponseTime();
+					writeObject(result);
+					return;
+				}
+				
+				Users user = new Users();
+				user.setCreatedate(System.currentTimeMillis());
+				user.setEmail(email);
+				user.setNickname(nickname);
+				user.setPassword(password);
+				user.setPoint(0);
+				DBMessage dbMsg = UsersManager.getInstance().insertObject(user);
+				
+				
 				if(dbMsg.getRow() > 0){
 					result.setMessage(Result.OK_MESSAGE);
 					result.setErrorCode(Result.ERROR_CODE_OK);
-					user.setPassword(null);
-					user.setDevice(null);
-					user.setGcm(null);
 					result.setResult(user);	
 				}else{
 					result.setMessage(dbMsg.getMessage());
 					result.setErrorCode(Result.ERROR_CODE_DB_ERROR);
-					result.setResult(dbMsg);
+					result.setResult(user);
+				}
+				result.makeResponseTime();
+				writeObject(result);
+				
+			}else{
+				result.setErrorCode(Result.ERROR_CODE_REQUEST_ERROR);
+				String message = null;
+				
+				if(!isValidEmail && !isValidPassword && isValidNickname){
+					message = "이메일 형식이 올바르지 않습니다. 비밀번호는 8자리 이상 16자리 미만입니다. 닉네임이 올바르지 않습니다.";
+				}else if(!isValidEmail){
+					message = "이메일 형식이 올바르지 않습니다.";
+				}else if(!isValidPassword){
+					message = "비밀번호는 8자리 이상 16자리 미만입니다.";
+				}else if(!isValidNickname){
+					message = "닉네임이 올바르지 않습니다.";
+				}
+				result.setMessage(message);
+				result.makeResponseTime();
+				writeObject(result);
+			}
+		}
+	}
+	
+	public void loginUser(){
+		if(checkRequestValidation()){
+			String email = getParameter(EMAIL);
+			String password = getParameter(PASSWORD);
+			String device = getParameter(DEVICE);
+			
+			boolean isValidEmail = UsersManager.isValidEmail(email);
+			boolean isValidPassword = UsersManager.isValidPassword(password);
+			boolean isValidDevice = device != null && device.length() > 0;
+			
+			Result result = new Result();
+			
+			if(isValidEmail && isValidPassword && isValidDevice){
+				Users user = UsersManager.getInstance().checkUser(email, password);
+				if(user != null){
+					/**
+					 * 일일 첫 방문 시 포인트 증가 로직.
+					 */
+					long currentConnTimestamp = System.currentTimeMillis();
+					long lastDays =  TimeUnit.MILLISECONDS.toDays(user.getLastconn());
+					long currentDays = TimeUnit.MILLISECONDS.toDays(currentConnTimestamp);
+					
+					if(currentDays > lastDays){
+						user.setPoint(user.getPoint() + 1);
+					}
+					
+					user.setLastconn(currentConnTimestamp);
+					user.setDevice(device);
+					DBMessage dbMsg = UsersManager.getInstance().updateObject(user);
+					if(dbMsg.getRow() > 0){
+						result.setMessage(Result.OK_MESSAGE);
+						result.setErrorCode(Result.ERROR_CODE_OK);
+						user.setPassword(null);
+						user.setDevice(null);
+						user.setGcm(null);
+						result.setResult(user);	
+					}else{
+						result.setMessage(dbMsg.getMessage());
+						result.setErrorCode(Result.ERROR_CODE_DB_ERROR);
+						result.setResult(dbMsg);
+					}
+				}else{
+					result.setMessage("아이디 또는 비밀번호가 올바르지 않습니다.");
+					result.setErrorCode(ERROR_CODE_INVALID_LOGIN_INFO);
 				}
 			}else{
-				result.setMessage("아이디 또는 비밀번호가 올바르지 않습니다.");
-				result.setErrorCode(ERROR_CODE_INVALID_LOGIN_INFO);
+				result.setErrorCode(Result.ERROR_CODE_REQUEST_ERROR);
+				String message = null;
+				
+				if(!isValidEmail && !isValidPassword && isValidDevice){
+					message = "이메일 형식이 올바르지 않습니다. 비밀번호는 8자리 이상 16자리 미만입니다. 닉네임이 올바르지 않습니다. 디바이스 정보가 없습니다.";
+				}else if(!isValidEmail){
+					message = "이메일 형식이 올바르지 않습니다.";
+				}else if(!isValidPassword){
+					message = "비밀번호는 8자리 이상 16자리 미만입니다.";
+				}else if(!isValidDevice){
+					message = "디바이스 정보가 없습니다.";
+				}
+				result.setMessage(message);
 			}
-		}else{
-			result.setErrorCode(Result.ERROR_CODE_REQUEST_ERROR);
-			String message = null;
-			
-			if(!isValidEmail && !isValidPassword && isValidDevice){
-				message = "이메일 형식이 올바르지 않습니다. 비밀번호는 8자리 이상 16자리 미만입니다. 닉네임이 올바르지 않습니다. 디바이스 정보가 없습니다.";
-			}else if(!isValidEmail){
-				message = "이메일 형식이 올바르지 않습니다.";
-			}else if(!isValidPassword){
-				message = "비밀번호는 8자리 이상 16자리 미만입니다.";
-			}else if(!isValidDevice){
-				message = "디바이스 정보가 없습니다.";
-			}
-			result.setMessage(message);
+			result.makeResponseTime();
+			writeObject(result);
 		}
-		result.makeResponseTime();
-		writeObject(result);
 	}
 	
 
