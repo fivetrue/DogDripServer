8eafabdb29dfecf7ac08691ac006cbc1f3a940f4
dudrpdjwls <dudrpdjwls@e9a08410-762d-0510-9cfe-9c7b6143a7ee>
2016-03-28 15:02:59 +0000

    GCM server 기능 추가.

---
+++ DogDIc/src/gcm/manager/NotificationManager.java
@@ -0,0 +1,37 @@
+package gcm.manager;
+
+import fivetrue.database.tables.Drips;
+import fivetrue.database.tables.Users;
+import gcm.data.MessageData;
+import gcm.sender.SendManager;
+
+public class NotificationManager {
+	
+	private static final int LIKE_DRIP_ID = 0x01;
+	
+	private static NotificationManager sInstance = null;
+	
+	public static NotificationManager getInstance(){
+		if(sInstance == null){
+			sInstance = new NotificationManager();
+		}
+		return sInstance;
+	}
+	
+	private NotificationManager(){
+		
+	}
+	
+	public void sendLikeNotification(Drips drip, Users author){
+		if(drip != null && author != null){
+			MessageData data = new MessageData();
+			data.setId(LIKE_DRIP_ID);
+			data.setCreatedate(System.currentTimeMillis());
+			data.setTitle("추천 알림");
+			data.setMessage("당신의 드립이 누군가에게 인정 되었습니다.");
+			data.setTargetObject(drip);
+			SendManager.getInstance().sendMessage(author.getGcm(), data);
+		}
+	}
+
+}
