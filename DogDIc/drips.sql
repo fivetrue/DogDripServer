e26e9466adb760cf859b8f9e614d5a71f4b5b8c7
dudrpdjwls <dudrpdjwls@e9a08410-762d-0510-9cfe-9c7b6143a7ee>
2016-03-30 09:17:10 +0000

    Drip table column 추가.

--- DogDIc/drips.sql
+++ DogDIc/drips.sql
@@ -4,6 +4,7 @@ use drip;
 CREATE TABLE `drip`.`drips` (
   `id` INT AUTO_INCREMENT,
   `drip` VARCHAR(512) NOT NULL,
+  `imageurl` VARCHAR(512) NOT NULL,
   `author` VARCHAR(45) NOT NULL,
   `createdate` BIGINT(12) NULL,
   `heartcount` INT NULL,
