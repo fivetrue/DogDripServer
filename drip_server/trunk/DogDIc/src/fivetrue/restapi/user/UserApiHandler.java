1b602b365155d618891a230ca2ca68636196a8e3
dudrpdjwls <dudrpdjwls@e9a08410-762d-0510-9cfe-9c7b6143a7ee>
2016-04-17 22:49:51 +0000

    자동 로그인 기능 추가 및 소스 배

--- drip_server/trunk/DogDIc/src/fivetrue/restapi/user/UserApiHandler.java
+++ drip_server/trunk/DogDIc/src/fivetrue/restapi/user/UserApiHandler.java
@@ -18,41 +18,43 @@ import fivetrue.restapi.Result;
 
 public class UserApiHandler extends BaseApiHandler{
 
-/**
- * 유저 errorcode 정보
- * 2000 = 중복된 이메일
- * 3000 = 중복된 닉네임
- * 400 = 파라메터 에러
- * 
- */
+	/**
+	 * 유저 errorcode 정보
+	 * 2000 = 중복된 이메일
+	 * 3000 = 중복된 닉네임
+	 * 400 = 파라메터 에러
+	 * 
+	 */
 	public static final String EMAIL = "email";
 	public static final String PASSWORD = "password";
 	public static final String NICKNAME = "nickname";
 	public static final String DEVICE = "device";
 	public static final String GCM = "gcm";
-	
+	public static final String LAST_CONN = "last_conn"; 
+	public static final String AUTO_LOGIN = "auto_login";
+
 	public static final int ERROR_CODE_DUPLICATED_EMAIL = 2000;
 	public static final int ERROR_CODE_DUPLICATED_NICKNAME = 2001;
 	public static final int ERROR_CODE_INVALID_LOGIN_INFO = 2002;
-	
+
 
 	public UserApiHandler(HttpServletRequest request, HttpServletResponse response) {
 		super(request, response);
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public void registerUser(){
 		if(checkRequestValidation()){
 			String email = getParameter(EMAIL);
 			String password = getParameter(PASSWORD);
 			String nickname = getParameter(NICKNAME);
-			
+
 			boolean isValidEmail = UsersManager.isValidEmail(email);
 			boolean isValidPassword = UsersManager.isValidPassword(password);
 			boolean isValidNickname = nickname != null && nickname.length() > 0;
-			
+
 			Result result = new Result();
-			
+
 			if(isValidEmail && isValidPassword && isValidNickname){
 				boolean checkEmail = UsersManager.getInstance().checkUserEmail(email);
 				if(!checkEmail){
@@ -62,7 +64,7 @@ public class UserApiHandler extends BaseApiHandler{
 					writeObject(result);
 					return;
 				}
-				
+
 				boolean checkNickname = UsersManager.getInstance().checkUserNickname(nickname);
 				if(!checkNickname){
 					result.setErrorCode(ERROR_CODE_DUPLICATED_NICKNAME);
@@ -71,7 +73,7 @@ public class UserApiHandler extends BaseApiHandler{
 					writeObject(result);
 					return;
 				}
-				
+
 				Users user = new Users();
 				user.setCreatedate(System.currentTimeMillis());
 				user.setEmail(email);
@@ -80,8 +82,8 @@ public class UserApiHandler extends BaseApiHandler{
 				user.setImageurl(Constants.DEFAULT_USER_IMAGE_THUNBNAIL);
 				user.setPoint(0);
 				DBMessage dbMsg = UsersManager.getInstance().insertObject(user);
-				
-				
+
+
 				if(dbMsg.getRow() > 0){
 					result.setMessage(Result.OK_MESSAGE);
 					result.setErrorCode(Result.ERROR_CODE_OK);
@@ -93,11 +95,11 @@ public class UserApiHandler extends BaseApiHandler{
 				}
 				result.makeResponseTime();
 				writeObject(result);
-				
+
 			}else{
 				result.setErrorCode(Result.ERROR_CODE_REQUEST_ERROR);
 				String message = null;
-				
+
 				if(!isValidEmail && !isValidPassword && isValidNickname){
 					message = "이메일 형식이 올바르지 않습니다. 비밀번호는 8자리 이상 16자리 미만입니다. 닉네임이 올바르지 않습니다.";
 				}else if(!isValidEmail){
@@ -113,78 +115,149 @@ public class UserApiHandler extends BaseApiHandler{
 			}
 		}
 	}
-	
+
 	public void loginUser(){
 		if(checkRequestValidation()){
 			String email = getParameter(EMAIL);
 			String password = getParameter(PASSWORD);
 			String gcm = getParameter(GCM);
-			
+			String autoLogin = getParameter(AUTO_LOGIN);
+			String lastConn = getParameter(LAST_CONN);
+
 			boolean isValidEmail = UsersManager.isValidEmail(email);
 			boolean isValidPassword = UsersManager.isValidPassword(password);
 			boolean isValidGcm = gcm != null && gcm.length() > 0;
-			
+			boolean bAUto = autoLogin != null && Boolean.getBoolean(autoLogin);
+			long lastConnection = lastConn != null && lastConn.length() > 0 ? Long.parseLong(lastConn) : 0;
+
 			Result result = new Result();
-			
-			if(isValidEmail && isValidPassword && isValidGcm){
-				Users user = UsersManager.getInstance().checkUser(email, password);
-				if(user != null){
-					/**
-					 * 일일 첫 방문 시 포인트 증가 로직.
-					 */
-					long currentConnTimestamp = System.currentTimeMillis();
-					long lastDays =  TimeUnit.MILLISECONDS.toDays(user.getLastconn());
-					long currentDays = TimeUnit.MILLISECONDS.toDays(currentConnTimestamp);
-					
-					if(currentDays > lastDays){
-						user.setPoint(user.getPoint() + 1);
-					}
-					
-					user.setLastconn(currentConnTimestamp);
-					user.setGcm(gcm);
-					DBMessage dbMsg = UsersManager.getInstance().updateObject(user);
-					if(dbMsg.getRow() > 0){
-						result.setMessage(Result.OK_MESSAGE);
-						result.setErrorCode(Result.ERROR_CODE_OK);
-						user.setPassword(null);
-						user.setGcm(null);
-						result.setResult(user);	
+
+			/**
+			 * 자동 로그인.
+			 */
+			if(bAUto){
+				if(isValidEmail && lastConnection > 0 && isValidGcm){
+					Users user = UsersManager.getInstance().getUser(email);
+					if(user != null){
+						boolean checkLastConn = user.getLastconn() == lastConnection;
+						if(checkLastConn){
+							/**
+							 * 일일 첫 방문 시 포인트 증가 로직.
+							 */
+							long currentConnTimestamp = System.currentTimeMillis();
+							long lastDays =  TimeUnit.MILLISECONDS.toDays(user.getLastconn());
+							long currentDays = TimeUnit.MILLISECONDS.toDays(currentConnTimestamp);
+
+							if(currentDays > lastDays){
+								user.setPoint(user.getPoint() + 1);
+							}
+
+							user.setLastconn(currentConnTimestamp);
+							user.setGcm(gcm);
+							DBMessage dbMsg = UsersManager.getInstance().updateObject(user);
+							if(dbMsg.getRow() > 0){
+								result.setMessage(Result.OK_MESSAGE);
+								result.setErrorCode(Result.ERROR_CODE_OK);
+								user.setPassword(null);
+								user.setGcm(null);
+								result.setResult(user);	
+							}else{
+								result.setMessage(dbMsg.getMessage());
+								result.setErrorCode(Result.ERROR_CODE_DB_ERROR);
+								result.setResult(dbMsg);
+							}
+
+							Gcm gcmInfo = GcmManager.getInstance().getGcmByGcm(gcm);
+							if(gcmInfo != null){
+								gcmInfo.setEmail(email);
+								GcmManager.getInstance().updateObject(gcmInfo);
+							}
+						}else{
+							result.setErrorCode(ERROR_CODE_INVALID_LOGIN_INFO);
+							result.setMessage("접속 정보가 올바르지 않습니다.");
+						}
 					}else{
-						result.setMessage(dbMsg.getMessage());
-						result.setErrorCode(Result.ERROR_CODE_DB_ERROR);
-						result.setResult(dbMsg);
+						result.setMessage("아이디가 올바르지 않습니다.");
+						result.setErrorCode(ERROR_CODE_INVALID_LOGIN_INFO);
 					}
-					
-					Gcm gcmInfo = GcmManager.getInstance().getGcmByGcm(gcm);
-					if(gcmInfo != null){
-						gcmInfo.setEmail(email);
-						GcmManager.getInstance().updateObject(gcmInfo);
+
+				}else{
+					result.setErrorCode(Result.ERROR_CODE_REQUEST_ERROR);
+					String message = null;
+					if(!isValidEmail && !isValidGcm){
+						message = "이메일 형식이 올바르지 않습니다.  GCM 정보가 없습니다.";
+					}else if(lastConnection <= 0){
+						message = "접속 정보가 올바르지 않습니다.";
+					}else if(!isValidGcm){
+						message = "GCM 정보가 없습니다.";
+					}
+					result.setMessage(message);
+				}
+			}
+			/**
+			 * 수동 로그인.
+			 */
+			else{
+				if(isValidEmail && isValidPassword && isValidGcm){
+					Users user = UsersManager.getInstance().checkUser(email, password);
+					if(user != null){
+						/**
+						 * 일일 첫 방문 시 포인트 증가 로직.
+						 */
+						long currentConnTimestamp = System.currentTimeMillis();
+						long lastDays =  TimeUnit.MILLISECONDS.toDays(user.getLastconn());
+						long currentDays = TimeUnit.MILLISECONDS.toDays(currentConnTimestamp);
+
+						if(currentDays > lastDays){
+							user.setPoint(user.getPoint() + 1);
+						}
+
+						user.setLastconn(currentConnTimestamp);
+						user.setGcm(gcm);
+						DBMessage dbMsg = UsersManager.getInstance().updateObject(user);
+						if(dbMsg.getRow() > 0){
+							result.setMessage(Result.OK_MESSAGE);
+							result.setErrorCode(Result.ERROR_CODE_OK);
+							user.setPassword(null);
+							user.setGcm(null);
+							result.setResult(user);	
+						}else{
+							result.setMessage(dbMsg.getMessage());
+							result.setErrorCode(Result.ERROR_CODE_DB_ERROR);
+							result.setResult(dbMsg);
+						}
+
+						Gcm gcmInfo = GcmManager.getInstance().getGcmByGcm(gcm);
+						if(gcmInfo != null){
+							gcmInfo.setEmail(email);
+							GcmManager.getInstance().updateObject(gcmInfo);
+						}
+					}else{
+						result.setMessage("아이디 또는 비밀번호가 올바르지 않습니다.");
+						result.setErrorCode(ERROR_CODE_INVALID_LOGIN_INFO);
 					}
 				}else{
-					result.setMessage("아이디 또는 비밀번호가 올바르지 않습니다.");
-					result.setErrorCode(ERROR_CODE_INVALID_LOGIN_INFO);
+					result.setErrorCode(Result.ERROR_CODE_REQUEST_ERROR);
+					String message = null;
+
+					if(!isValidEmail && !isValidPassword && !isValidGcm){
+						message = "이메일 형식이 올바르지 않습니다. 비밀번호는 8자리 이상 16자리 미만입니다. 닉네임이 올바르지 않습니다. GCM 정보가 없습니다.";
+					}else if(!isValidEmail){
+						message = "이메일 형식이 올바르지 않습니다.";
+					}else if(!isValidPassword){
+						message = "비밀번호는 8자리 이상 16자리 미만입니다.";
+					}else if(!isValidGcm){
+						message = "GCM 정보가 없습니다.";
+					}
+					result.setMessage(message);
 				}
-			}else{
-				result.setErrorCode(Result.ERROR_CODE_REQUEST_ERROR);
-				String message = null;
-				
-				if(!isValidEmail && !isValidPassword && isValidGcm){
-					message = "이메일 형식이 올바르지 않습니다. 비밀번호는 8자리 이상 16자리 미만입니다. 닉네임이 올바르지 않습니다. GCM 정보가 없습니다.";
-				}else if(!isValidEmail){
-					message = "이메일 형식이 올바르지 않습니다.";
-				}else if(!isValidPassword){
-					message = "비밀번호는 8자리 이상 16자리 미만입니다.";
-				}else if(!isValidGcm){
-					message = "GCM 정보가 없습니다.";
-				}
-				result.setMessage(message);
 			}
 			result.makeResponseTime();
 			writeObject(result);
 		}
 	}
-	
-	
+
+
 	public void registerGcm(){
 		if(checkRequestValidation()){
 			String id = getParameter(GCM);
@@ -192,7 +265,7 @@ public class UserApiHandler extends BaseApiHandler{
 			boolean isValidGcm = id != null && id.length() > 0;
 			boolean isValidDevice = device != null && device.length() > 0;
 			Result result = new Result();
-			
+
 			if(isValidGcm && isValidDevice){
 				Gcm gcm = GcmManager.getInstance().getGcmByGcm(id);
 				if(gcm == null){
@@ -215,7 +288,7 @@ public class UserApiHandler extends BaseApiHandler{
 				result.setErrorCode(Result.ERROR_CODE_REQUEST_ERROR);
 				result.setMessage("Gcm Id 또는 Device 정보가 올바르지 않습니다.");
 			}
-			
+
 			result.makeResponseTime();
 			writeObject(result);
 		}
