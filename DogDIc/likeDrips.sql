ef2ef42d5adeccab3ce00a8dcea74271413b2fa5
dudrpdjwls <dudrpdjwls@e9a08410-762d-0510-9cfe-9c7b6143a7ee>
2016-05-13 17:02:51 +0000

    query update

--- DogDIc/likeDrips.sql
+++ DogDIc/likeDrips.sql
@@ -5,7 +5,8 @@ CREATE TABLE `drip`.`likedrips` (
   `likeid` INT AUTO_INCREMENT,
   `dripid` INT,
   `userid` VARCHAR(45) NOT NULL,
-  `likedate` BIGINT(12) NULL);
+  `likedate` BIGINT(12) NULL,
+  PRIMARY KEY (`likeid`));
 
   
   
--- DogDIc/reply.sql
+++ DogDIc/reply.sql
@@ -7,6 +7,6 @@ CREATE TABLE `drip`.`reply` (
   `comment` VARCHAR(200) NOT NULL,
   `userid` VARCHAR(45) NOT NULL,
   `replydate` BIGINT(12) NULL,
-  PRIMARY KEY (`id`));
+  PRIMARY KEY (`replyid`));
 
  
